164b19904b06778eb8bd01ff962fc2f44cbed524
Poyo663 <jeffersondeazevedomaciel@gmail.com>
2023-12-14 09:35:34 -0300

    restarting the project

--- index.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-
-app.use(express.static('./public'))
-
-app.listen(process.env.PORT, () => {
-    console.log(`listening in port ${process.env.PORT}`)
-})
